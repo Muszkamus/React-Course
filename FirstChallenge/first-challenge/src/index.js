5a13af149825179652e2a289e09bdd20f8da5cba
Radek <balicki56@gmail.com>
2025-05-12 22:59:20 +0100

    Challenge completed

--- FirstChallenge/first-challenge/src/index.js
+++ FirstChallenge/first-challenge/src/index.js
@@ -2,35 +2,57 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const skills = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React",
+  " BackEnd",
+  "FrontEnd",
+  "Quantum physics",
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <MainBox />
   </React.StrictMode>
 );
 
 function App() {
-  return <div className="main"></div>;
+  return <MainBox />;
 }
 function MainBox() {
   return (
     <div className="mainBox">
-      <div className="picture">Picture</div>
+      <div className="pictureBox">
+        <img className="picture1" src="/picture1.jpg" alt="pic1"></img>
+      </div>
       <h1 className="title">Radoslaw Balicki</h1>
-      <h2 className="description">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      <p className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
-      </h2>
+      </p>
       <SkillSet />
     </div>
   );
 }
 function SkillSet() {
-  <Skills name="JavaScript" />;
-  return <div className="skillset"></div>;
+  return (
+    <div className="skillset">
+      {skills.map((n) => (
+        <Skill name={n} />
+      ))}
+    </div>
+  );
 }
 
-function Skills(props) {}
+function Skill(props) {
+  return (
+    <div className="skill">
+      <p>{props.name}</p>
+    </div>
+  );
+}
