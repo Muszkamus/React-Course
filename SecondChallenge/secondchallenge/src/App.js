41fd04557aa6402eff3ffe6040a1f8f6779144e7
Radek <balicki56@gmail.com>
2025-05-18 11:36:54 +0100

    challenge done to be soon

--- SecondChallenge/secondchallenge/src/App.js
+++ SecondChallenge/secondchallenge/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+
+  const date = new Date(2025, 4, 18);
+  const numericalDate = date.getTime();
+  const day = 1000 * 60 * 60 * 24 * count;
+
+  const countDay = new Date(numericalDate + day);
+
+  function handleNextStep() {
+    setStep((s) => s + 1);
+  }
+
+  function handlePreviousStep() {
+    setStep((s) => s - 1);
+  }
+
+  function handleNextCount() {
+    setCount((s) => s + 1);
+  }
+
+  function handlePreviousCount() {
+    setCount((s) => s - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="settings">
+        <button className="button" onClick={() => handlePreviousStep()}>
+          -
+        </button>
+
+        <p className="stepCounter">Step: {step}</p>
+        <button className="button" onClick={() => handleNextStep()}>
+          +
+        </button>
+      </div>
+      <div>
+        <div className="settings">
+          <button className="button" onClick={() => handlePreviousCount()}>
+            -
+          </button>
+
+          <p className="stepCounter">Count: {count}</p>
+          <button className="button" onClick={() => handleNextCount()}>
+            +
+          </button>
+        </div>
+      </div>
+      <div className="timer">
+        <p>{countDay.toString()}</p>
+      </div>
     </div>
   );
 }
-
-export default App;
