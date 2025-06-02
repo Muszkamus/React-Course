de727df6e83efdc7d4afb9ef63f655cd6d918469
Radek <balicki56@gmail.com>
2025-06-02 21:22:04 +0100

    Workimg on eat-n-split

--- Challenges/tip-calculator-challenge/src/App.js
+++ Challenges/tip-calculator-challenge/src/App.js
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
 const serviceData = [
   {
@@ -9,46 +9,65 @@ const serviceData = [
   {
     id: 2,
     text: "It was okay (5%)",
-    value: 5,
+    value: 0.05,
   },
   {
     id: 3,
     text: "It was good (10%)",
-    value: 10,
+    value: 0.1,
   },
   {
     id: 4,
     text: "Absolutely Amazing! (20%)",
-    value: 20,
+    value: 0.2,
   },
 ];
 
 export default function App() {
-  const [bill, setBill] = useState(null);
-  const [service, setService] = useState("");
-  const [friend, setFriend] = useState("");
+  const [bill, setBill] = useState(0);
+  const [service, setService] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
+
+  function resetAll() {
+    setBill(0);
+    setService(0);
+    setFriendRating(0);
+  }
+
   return (
     <div>
       <Bill bill={bill} setBill={setBill} />
       <ServiceRating
         serviceData={serviceData}
-        service={Number(service)}
+        service={service}
         setService={setService}
       />
       <FriendRecommendation
         serviceData={serviceData}
-        friend={Number(friend)}
-        setFriend={setFriend}
+        friendRating={friendRating}
+        setFriendRating={setFriendRating}
       />
       <TotalBill
-        bill={Number(bill)}
+        bill={bill}
         setBill={setBill}
         serviceData={serviceData}
-        service={Number(service)}
-        friend={Number(friend)}
-        setFriend={setFriend}
+        service={service}
+        friendRating={friendRating}
+        setFriendRating={setFriendRating}
+        resetAll={resetAll}
       />
     </div>
+  );
+}
+function DropDown({ value, setValue }) {
+  return (
+    <select onChange={(e) => setValue(Number(e.target.value))} value={value}>
+      {serviceData.map((i) => (
+        <option key={i.id} value={i.value}>
+          {i.text}
+        </option>
+      ))}
+    </select>
   );
 }
 
@@ -68,53 +87,39 @@ function Bill({ bill, setBill }) {
   );
 }
 
-function ServiceRating({ serviceData, setService }) {
+function ServiceRating({ service, setService }) {
   return (
     <div>
       <p className="text">
         How much did you like the service?
-        <select onChange={(e) => setService(e.target.value)}>
-          {serviceData.map((i) => (
-            <option key={i.id} value={i.value}>
-              {i.text}
-            </option>
-          ))}
-        </select>
+        <DropDown value={service} setValue={setService} />
       </p>
     </div>
   );
 }
 
-function FriendRecommendation({ serviceData, setFriend }) {
+function FriendRecommendation({ friendRating, setFriendRating }) {
   return (
     <div>
       <p className="text">
         How much did your friend like the service?
-        <select onChange={(e) => setFriend(e.target.value)}>
-          {serviceData.map((i) => (
-            <option key={i.id} value={i.value}>
-              {i.text}
-            </option>
-          ))}
-        </select>
+        <DropDown value={friendRating} setValue={setFriendRating} />
       </p>
     </div>
   );
 }
-function TotalBill({ bill, setBill, service, setService, friend, setFriend }) {
-  // Reset button to do
-  function resetAll() {
-    setBill(0);
-    setService(0);
-    setFriend(0);
-  }
+function TotalBill({ bill, service, friendRating, resetAll }) {
+  const numericBill = Number(bill);
+  const finalService = numericBill * service;
+  const finalFriendRating = numericBill * friendRating;
+
   return (
     <div>
       <p className="textBill">
-        You Pay - £{bill} (£{bill} +
-        {(bill * service) / 100 + (bill * friend) / 100})
+        You Pay - £{numericBill === "" ? "0" : numericBill} (£
+        {numericBill === "" ? "0" : numericBill} +
+        {(finalService + finalFriendRating).toFixed(2)} tip)
       </p>
-
       <button onClick={resetAll}>Reset</button>
     </div>
   );
--- Notes/Notes.md
+++ Notes/Notes.md
@@ -1986,3 +1986,143 @@ function AccordionItem({ num, title, curOpen, onOpen, children }) {
 ```
 
 ---
+
+# 93. **Challenge- Tip Calculator**
+
+---
+
+```js
+import { useState } from "react";
+
+const serviceData = [
+  {
+    id: 1,
+    text: "Dissatisfied (0%)",
+    value: 0,
+  },
+  {
+    id: 2,
+    text: "It was okay (5%)",
+    value: 0.05,
+  },
+  {
+    id: 3,
+    text: "It was good (10%)",
+    value: 0.1,
+  },
+  {
+    id: 4,
+    text: "Absolutely Amazing! (20%)",
+    value: 0.2,
+  },
+];
+
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [service, setService] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
+
+  function resetAll() {
+    setBill(0);
+    setService(0);
+    setFriendRating(0);
+  }
+
+  return (
+    <div>
+      <Bill bill={bill} setBill={setBill} />
+      <ServiceRating
+        serviceData={serviceData}
+        service={service}
+        setService={setService}
+      />
+      <FriendRecommendation
+        serviceData={serviceData}
+        friendRating={friendRating}
+        setFriendRating={setFriendRating}
+      />
+      <TotalBill
+        bill={bill}
+        setBill={setBill}
+        serviceData={serviceData}
+        service={service}
+        friendRating={friendRating}
+        setFriendRating={setFriendRating}
+        resetAll={resetAll}
+      />
+    </div>
+  );
+}
+function DropDown({ value, setValue }) {
+  return (
+    <select onChange={(e) => setValue(Number(e.target.value))} value={value}>
+      {serviceData.map((i) => (
+        <option key={i.id} value={i.value}>
+          {i.text}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function Bill({ bill, setBill }) {
+  return (
+    <div>
+      <p className="text">
+        How much was the bill?
+        <input
+          type="number"
+          placeholder=""
+          value={bill}
+          onChange={(e) => setBill(e.target.value)}
+        ></input>
+      </p>
+    </div>
+  );
+}
+
+function ServiceRating({ service, setService }) {
+  return (
+    <div>
+      <p className="text">
+        How much did you like the service?
+        <DropDown value={service} setValue={setService} />
+      </p>
+    </div>
+  );
+}
+
+function FriendRecommendation({ friendRating, setFriendRating }) {
+  return (
+    <div>
+      <p className="text">
+        How much did your friend like the service?
+        <DropDown value={friendRating} setValue={setFriendRating} />
+      </p>
+    </div>
+  );
+}
+function TotalBill({ bill, service, friendRating, resetAll }) {
+  const numericBill = Number(bill);
+  const finalService = numericBill * service;
+  const finalFriendRating = numericBill * friendRating;
+
+  return (
+    <div>
+      <p className="textBill">
+        You Pay - £{numericBill === "" ? "0" : numericBill} (£
+        {numericBill === "" ? "0" : numericBill} +{(
+          finalService + finalFriendRating
+        ).toFixed(2)} tip)
+      </p>
+      <button onClick={resetAll}>Reset</button>
+    </div>
+  );
+}
+```
+
+---
+
+# <centre> **Section 8. Practice Project: Eat-N-Split**
+
+---
