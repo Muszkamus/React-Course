a09efcfb519ddab14fcc092e697188a34b80f508
Radek <balicki56@gmail.com>
2025-08-11 23:36:08 +0100

    Including a Map With the Leaflet Library

--- 11-worldwise/worldwise/src/components/Map.jsx
+++ 11-worldwise/worldwise/src/components/Map.jsx
@@ -1,9 +1,13 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
+  const [mapPosition, setMapPosition] = useState([51.505, 0]);
+
   const navigate = useNavigate();
-  const [searchParams, setSearchParems] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
@@ -13,13 +17,19 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParems({ lat: 23, lng: 50 })}>
-        Change Position
-      </button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        style={{ height: 400 }}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={mapPosition}></Marker>
+      </MapContainer>
     </div>
   );
 }
