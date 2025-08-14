35946fc6862a51761a01b2e0902db68245d28298
Radek <balicki56@gmail.com>
2025-08-14 23:44:04 +0100

    Creating a New City

--- 11-worldwise/worldwise/src/components/Form.jsx
+++ 11-worldwise/worldwise/src/components/Form.jsx
@@ -1,5 +1,5 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
-
+import "react-datepicker/dist/react-datepicker.css";
 import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
@@ -8,6 +8,9 @@ import Message from "./Message";
 import BackButton from "./BackButton";
 import Spinner from "./Spinner";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import DatePicker from "react-datepicker";
+import { useCities } from "../contexts/CitiesContext";
+import { useNavigate } from "react-router-dom";
 
 // import BackButton from "./BackButton";
 
@@ -21,6 +24,8 @@ export function convertToEmoji(countryCode) {
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 function Form() {
   const [lat, lng] = useUrlPosition();
+  const { createCity, isLoading } = useCities();
+  const navigate = useNavigate();
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
@@ -39,7 +44,7 @@ function Form() {
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
-          console.log(data);
+
           if (!data.countryCode)
             throw new Error("That doesn't seem to be a city");
           setCityName(data.city || data.locality || "");
@@ -56,12 +61,33 @@ function Form() {
     [lat, lng]
   );
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
   if (isLoadingGeocoding) return <Spinner />;
+  if (!lat && !lng)
+    return <Message message={"Start by clicking somewher on the map"} />;
 
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -74,10 +100,15 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id="date"
           onChange={(e) => setDate(e.target.value)}
           value={date}
+        /> */}
+        <DatePicker
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat={"dd/MM/yyyy"}
         />
       </div>
 
--- 11-worldwise/worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/worldwise/src/contexts/CitiesContext.jsx
@@ -40,9 +40,33 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      setCities((cities) => [...cities, data]);
+    } catch {
+      alert("There was an error fetching");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, setCurrentCity, getCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        setCurrentCity,
+        getCity,
+        createCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
