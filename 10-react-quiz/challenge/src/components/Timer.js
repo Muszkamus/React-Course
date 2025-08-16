e4b03effbfb4934af2449d9e68755433eb633323
Radek <balicki56@gmail.com>
2025-08-16 21:40:35 +0100

    starting challenge initial state

---
+++ 10-react-quiz/challenge/src/components/Timer.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  // Very useful for showing time
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+        //console.log("tick");
+      }, 10000);
+
+      return () => clearInterval(id); // clean up function
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {/* Time formatting */}
+      {mins < 10 ? 0 : null}
+      {mins}:{seconds < 10 ? 0 : null}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
