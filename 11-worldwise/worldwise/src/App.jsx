0e1317eb982d622b13fec88bfce357bcddbae8b3
Radek <balicki56@gmail.com>
2025-08-16 20:35:34 +0100

    Adding Fake Authentication: Protecting a Route

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -12,6 +12,7 @@ import Form from "./components/Form";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import "./index.css";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -23,7 +24,15 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />{" "}
+                </ProtectedRoute>
+              }
+            >
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
               <Route
