a3b3bdab706f363d5ce35274dbd1a971e1b3d1cd
Radek <balicki56@gmail.com>
2025-09-04 23:33:01 +0100

    brushing up

--- 16-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+
+  { path: "/order/:orderID", element: <Order /> },
+]);
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
