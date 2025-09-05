6f97b16c3228cf422e4a5f48c6fb0b4076130485
Radek <balicki56@gmail.com>
2025-09-06 00:18:09 +0100

    290. Fetching Orders

---
+++ 16-fast-react-pizza/react-pizza/src/features/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+import { useLoaderData } from "react-router-dom";
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
