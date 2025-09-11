758f289bb13a499a777594661f33bedd64cd6a8c
Radek <balicki56@gmail.com>
2025-09-12 00:05:24 +0100

    307. Configuring Tailwind: Custom Font Family

--- 16-fast-react-pizza/react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/react-pizza/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 👈 this is for Tailwind to scan your React files
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // This is for Tailwind to scan React files
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace', // Sans (Whole project)
+      //  specificContent: 'Roboto Mono, monospace' // Could be used for specific places
+    },
+
+    extend: {
+      colors: { title: '#123456' }, // This can be used for specific content as well
+    },
   },
   plugins: [],
 };
